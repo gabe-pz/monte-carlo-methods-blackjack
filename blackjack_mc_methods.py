78c3200dc58642c1b4484ce859855b4ae38c9ef8
Gabe Perez <gperez58857@gmail.com>
2025-12-28 19:29:01 -0800

    Started formulation for blackjack into a finite MDP

--- blackjack_mc_methods.py
+++ blackjack_mc_methods.py
@@ -9,6 +9,9 @@ def fixed_policy(player_hand, dealer_up_card):
         return 'H'
 
 def run_episode():
+    states = [] 
+    actions = [] 
+    
     player_goes_bust = False
     dealer_goes_bust = False
         
@@ -22,39 +25,43 @@ def run_episode():
     player_hand = evaluate_inital_hand(player_card_1, player_card_2) 
     dealer_hand = evaluate_inital_hand(dealer_down_card, dealer_up_card)
 
-    if(player_hand[0] == 21 and dealer_hand[0] == 21): 
-        return 0
+    if(player_hand[0] > 11):
+        states.append((player_hand[0], dealer_up_card[0], player_hand[1])) 
+    
+    if(player_hand[0] == 21 and dealer_hand[0] == 21):     
+        return states, actions, 0
     
     if(player_hand[0] == 21):
-        return 1 
+        return states, actions, 1
     
     if(dealer_hand[0] == 21):
-        return -1
-        
+        return states, actions, -1
+    
+    #Always hit between 2-11 
+    while player_hand[0] < 12:
+        player_card_dealt = create_and_shuffle_deck()[0]  
+
+        player_hand = evaluate_hand(player_hand, player_card_dealt) 
+
     #Players Turn
     while True:
         policy_choice = fixed_policy(player_hand, dealer_up_card[0])  
-        
-        #Always hit between 2-11
-        if(player_hand[0] < 12):
-            player_card_dealt = create_and_shuffle_deck()[0]  
 
-            player_hand = evaluate_hand(player_hand, player_card_dealt) 
-
-            if(player_hand[0] > 21): 
-                player_goes_bust = True
-                break 
         #Policy choice for 12-21
-        elif(policy_choice == 'H'):
+        if(policy_choice == 'H'):
             player_card_dealt = create_and_shuffle_deck()[0]  
 
             player_hand = evaluate_hand(player_hand, player_card_dealt) 
 
+            states.append((player_hand[0], dealer_up_card[0], player_hand[1]))   
+            actions.append('H') 
             if(player_hand[0] > 21): 
                 player_goes_bust = True
                 break 
 
         else:
+            states.append((player_hand[0], dealer_up_card[0], player_hand[1]))  
+            actions.append('S')  
             break 
 
     #Dealers Turn     
@@ -70,29 +77,25 @@ def run_episode():
         
     #Evaluate results
     if player_goes_bust:
-        return -1
+        return states, actions, -1
     if dealer_goes_bust:
-        return 1 
+        return states, actions, 1
     
     #Checking who is close to 21
     if player_hand[0] > dealer_hand[0]:
-        return 1
+        return states, actions, 1
     elif player_hand[0] < dealer_hand[0]:
-        return -1
+        return states, actions, -1
     else:
-        return 0
-    
-def state_value_function(state):
-    return 
-
-def first_visit_mc_prediction():
-    state_value_functions = [] 
-    
+        return states, actions, 0 
 
 if __name__ == '__main__':
 
     state_space = [] 
+    state_values = [] 
+    returns = []
 
+    #Create the state space
     for bool in [True, False]: 
         for i in range(12, 22):
             for j in range(2, 11):
@@ -101,9 +104,13 @@ if __name__ == '__main__':
                     state_space.append((i, j, bool))
                 else:
                     state_space.append((i, j, bool))
-        
-
+    
+    #Initalize state value functions
     for state in state_space:
-        print(state) 
-    print(len(state_space)) 
-     
+        state_values.append(randint(-1, 1)) 
+
+    states_ep, actions_ep, reward_ep = run_episode() 
+
+    print(states_ep)  
+    print(actions_ep)
+    print(reward_ep)  
