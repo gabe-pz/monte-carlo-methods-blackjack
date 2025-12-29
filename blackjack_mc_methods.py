a7e1fcb4518d9d4022958dc5ce0aa490a1af4cf9
Gabe Perez <gperez58857@gmail.com>
2025-12-29 13:14:08 -0800

    Getting the run episode code and main function code ready to evaluate fixed policy

--- blackjack_mc_methods.py
+++ blackjack_mc_methods.py
@@ -1,9 +1,9 @@
 from blackjack_logic_functions import evaluate_hand, evaluate_inital_hand, create_and_shuffle_deck 
 from random import randint 
 
-def fixed_policy(player_hand, dealer_up_card): 
+def fixed_policy(state): 
 
-    if(player_hand[0] > 19):
+    if(state[0] > 19):
         return 'S'
     else:
         return 'H'
@@ -25,16 +25,20 @@ def run_episode():
     player_hand = evaluate_inital_hand(player_card_1, player_card_2) 
     dealer_hand = evaluate_inital_hand(dealer_down_card, dealer_up_card)
 
-    if(player_hand[0] > 11):
+
+    if(player_hand[0] == 21 and dealer_hand[0] == 21):  
         states.append((player_hand[0], dealer_up_card[0], player_hand[1])) 
-    
-    if(player_hand[0] == 21 and dealer_hand[0] == 21):     
+        actions.append(' ')   
         return states, actions, 0
     
     if(player_hand[0] == 21):
+        states.append((player_hand[0], dealer_up_card[0], player_hand[1])) 
+        actions.append(' ')   
         return states, actions, 1
     
     if(dealer_hand[0] == 21):
+        states.append((player_hand[0], dealer_up_card[0], player_hand[1])) 
+        actions.append(' ')   
         return states, actions, -1
     
     #Always hit between 2-11 
@@ -42,10 +46,14 @@ def run_episode():
         player_card_dealt = create_and_shuffle_deck()[0]  
 
         player_hand = evaluate_hand(player_hand, player_card_dealt) 
+    
+    #Note the inital playable state
+    states.append((player_hand[0], dealer_up_card[0], player_hand[1])) 
+    i = 0
 
     #Players Turn
     while True:
-        policy_choice = fixed_policy(player_hand, dealer_up_card[0])  
+        policy_choice = fixed_policy(states[i])   
 
         #Policy choice for 12-21
         if(policy_choice == 'H'):
@@ -55,12 +63,15 @@ def run_episode():
 
             states.append((player_hand[0], dealer_up_card[0], player_hand[1]))   
             actions.append('H') 
+            #Gone to next state
+            i += 1
             if(player_hand[0] > 21): 
                 player_goes_bust = True
+                #Over 21 and states are only, 12-21 
+                states.pop()
                 break 
 
         else:
-            states.append((player_hand[0], dealer_up_card[0], player_hand[1]))  
             actions.append('S')  
             break 
 
@@ -88,12 +99,18 @@ def run_episode():
         return states, actions, -1
     else:
         return states, actions, 0 
+def average_list(lst):
+    sum = 0
+    for element in lst:
+        sum += element 
+    return (sum/len(lst)) 
 
 if __name__ == '__main__':
-
     state_space = [] 
-    state_values = [] 
-    returns = []
+    state_values = {}
+    returns = {} 
+
+    episodes = 1000
 
     #Create the state space
     for bool in [True, False]: 
@@ -105,12 +122,20 @@ if __name__ == '__main__':
                 else:
                     state_space.append((i, j, bool))
     
-    #Initalize state value functions
+    #Initalize state value functions and creating empty list to store returns corresponding to each state
     for state in state_space:
-        state_values.append(randint(-1, 1)) 
+        state_values[state] = randint(-1, 1) 
+        returns[state] = [] 
 
-    states_ep, actions_ep, reward_ep = run_episode() 
+    
+    #Policy Evaulation
+    for i in range(episodes): 
+        states_ep, actions_ep, return_ep = run_episode()
 
-    print(states_ep)  
-    print(actions_ep)
-    print(reward_ep)  
+        print(actions_ep)
+        print(return_ep)
+        print() 
+
+
+
+
